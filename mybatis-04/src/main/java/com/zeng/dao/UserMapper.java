af69e88d4421cb4941f4dd3662c9712c0fd8234d
danger.zeng <danger.zeng@qq.com>
2021-03-14 14:44:19 +0800

    Mybatis-04:用log4j日志显示

--- mybatis-04/src/main/java/com/zeng/dao/UserMapper.java
+++ mybatis-04/src/main/java/com/zeng/dao/UserMapper.java
@@ -2,6 +2,10 @@ package com.zeng.dao;
 
 import com.zeng.pojo.User;
 
+import java.util.List;
+
 public interface UserMapper {
     User getUserById(int id);
+
+
 }
--- mybatis-04/src/test/java/com/zeng/dao/UserDaoTest.java
+++ mybatis-04/src/test/java/com/zeng/dao/UserDaoTest.java
@@ -1,14 +1,27 @@
 package com.zeng.dao;
 
 import com.zeng.pojo.User;
-import com.zeng.utils.MybatisUtils;
+import com.zeng.untils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.junit.Test;
+import com.zeng.untils.initLogRecord;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
 
 public class UserDaoTest {
 
+    static Logger logger = Logger.getLogger(UserDaoTest.class);
     @Test
-    public void test(){
+    public void test() throws FileNotFoundException {
+        logger.info("info：进入selectUser方法");
+        logger.debug("debug：进入selectUser方法");
+        logger.error("error: 进入selectUser方法");
         SqlSession sqlSession= MybatisUtils.getSqlSession();
         UserMapper userDao=sqlSession.getMapper(UserMapper.class);
         User user=userDao.getUserById(3);
