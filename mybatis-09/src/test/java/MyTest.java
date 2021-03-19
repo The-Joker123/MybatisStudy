47cf489d442f8d1f2ad81203081d00b3827bcd39
danger.zeng <danger.zeng@qq.com>
2021-03-19 11:06:00 +0800

    Mybatis-09:二级Catch缓存测试

--- mybatis-09/src/test/java/MyTest.java
+++ mybatis-09/src/test/java/MyTest.java
@@ -15,7 +15,11 @@ public class MyTest {
     //一级缓存SqlSession测试
     public void testqueryBlog(){
         SqlSession sqlSession = MybatisUtils.getSqlSession();
+        SqlSession sqlSession2 = MybatisUtils.getSqlSession();
+        SqlSession sqlSession3= MybatisUtils.getSqlSession();
         BlogMapper blogMapper = sqlSession.getMapper(BlogMapper.class);
+        BlogMapper blogMapper2= sqlSession2.getMapper(BlogMapper.class);
+        BlogMapper blogMapper3= sqlSession3.getMapper(BlogMapper.class);
 
         HashMap<String,String> hashMap=new HashMap<String,String>();
 //        hashMap.put("title","你好");
@@ -24,15 +28,26 @@ public class MyTest {
         for (Blog blog : blogList) {
             System.out.println(blog);
         }
+        sqlSession.close();
         System.out.println("================================================");
         HashMap<String,String> hashMap1=new HashMap<String,String>();
 //        hashMap.put("title","你好");
 //        hashMap.put("author","曾自喜");
-        List<Blog> blogList1 = blogMapper.queryBlogif(hashMap);
+        List<Blog> blogList1 = blogMapper2.queryBlogif(hashMap);
         for (Blog blog : blogList1) {
             System.out.println(blog);
         }
+        sqlSession2.close();
+        System.out.println("================================================");
+        HashMap<String,String> hashMap2=new HashMap<String,String>();
+//        hashMap.put("title","你好");
+//        hashMap.put("author","曾自喜");
+        List<Blog> blogList3 = blogMapper3.queryBlogif(hashMap);
+        for (Blog blog : blogList3) {
+            System.out.println(blog);
+        }
+        sqlSession2.close();
 
-        sqlSession.close();
+
     }
 }
